c631e686fc5d292c715ec4ad56c5e114ef970968
Oleg Pudeyev <oleg@bsdpower.com>
2018-12-21 16:50:02 -0500

    command monitoring logging

--- spec/lite_spec_helper.rb
+++ spec/lite_spec_helper.rb
@@ -115,3 +115,45 @@ RSpec.configure do |config|
 end
 
 EventSubscriber.initialize
+
+class CommandLogSubscriber
+  include Mongo::Loggable
+
+  def started(event)
+    log_warn("#{prefix(event)} | STARTED | #{format_command(event.command)}")
+  end
+
+  def succeeded(event)
+    log_warn("#{prefix(event)} | SUCCEEDED | #{event.duration}s")
+  end
+
+  def failed(event)
+    log_warn("#{prefix(event)} | FAILED | #{event.message} | #{event.duration}s")
+  end
+
+  private
+
+  def logger
+    Mongo::Logger.logger
+  end
+
+  def format_command(args)
+    begin
+      args.inspect
+    rescue Exception
+      '<Unable to inspect arguments>'
+    end
+  end
+
+  def format_message(message)
+    format("COMMAND | %s".freeze, message)
+  end
+
+  def prefix(event)
+    "#{event.address.to_s} | #{event.database_name}.#{event.command_name}"
+  end
+end
+
+subscriber = CommandLogSubscriber.new
+
+Mongo::Monitoring::Global.subscribe(Mongo::Monitoring::COMMAND, subscriber)
