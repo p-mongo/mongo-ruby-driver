e696effd08cff3ef1054730e06fe963b3f8a689b
Oleg Pudeyev <oleg@bsdpower.com>
2019-02-20 23:31:37 -0500

    1572

---
+++ spec/integration/stepdown_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'Stepdown behavior' do
+  describe 'getMore iteration' do
+    let(:collection) { authorized_client['stepdown'] }
+
+    before do
+      collection.insert_many([{test: 1}] * 100)
+    end
+
+    let(:view) { collection.find({test: 1}, batch_size: 10) }
+    let(:enum) { view.to_enum }
+
+    it 'continues through stepdown' do
+      # get the first item
+      item = enum.next
+      expect(item['test']).to eq(1)
+
+      ClusterTools.instance.change_primary
+
+      # exhaust the batch
+      9.times do
+        enum.next
+      end
+
+      # this should issue a getMore
+      item = enum.next
+      expect(item['test']).to eq(1)
+    end
+  end
+end
