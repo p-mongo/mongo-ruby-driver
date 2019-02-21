0456b76cf362196f13528c0db728ab077afa8b12
Oleg Pudeyev <oleg@bsdpower.com>
2019-02-21 10:37:33 -0500

    1572

--- spec/integration/stepdown_spec.rb
+++ spec/integration/stepdown_spec.rb
@@ -52,4 +52,65 @@ describe 'Stepdown behavior' do
       expect(get_more_socket_object_id).to eq(find_socket_object_id)
     end
   end
+
+  describe 'writes on connections' do
+    let(:server) { authorized_client.with(app_name: rand).cluster.next_primary }
+    let(:connection) { server.pool.checkout }
+    let(:operation) do
+      Mongo::Operation::Insert::OpMsg.new(
+        documents: [{test: 1}],
+        db_name: SpecConfig.instance.test_db,
+        coll_name: 'stepdown',
+        write_concern: Mongo::WriteConcern.get(write_concern),
+      )
+    end
+    let(:message) { operation.send(:message, server) }
+    let(:first_message) do
+      Mongo::Operation::Insert::OpMsg.new(
+        documents: [{test: 1}],
+        db_name: SpecConfig.instance.test_db,
+        coll_name: 'stepdown',
+        write_concern: Mongo::WriteConcern.get(w: 1),
+      ).send(:message, server)
+    end
+
+    after do
+      server.pool.checkin(connection)
+    end
+
+    describe 'acknowledged write after stepdown' do
+      let(:write_concern) { {:w => 1} }
+
+      it 'keeps connection open' do
+        rv = connection.dispatch([first_message], 1)
+        expect(rv.documents.first['ok']).to eq(1)
+
+        ClusterTools.instance.change_primary
+
+        rv = connection.dispatch([message], 1)
+        doc = rv.documents.first
+        expect(doc['ok']).to eq(0)
+        expect(doc['codeName']).to eq('NotMaster')
+
+        expect(connection.connected?).to be true
+      end
+    end
+
+    describe 'unacknowledged write after stepdown' do
+      let(:write_concern) { {:w => 0} }
+
+      it 'closes the connection' do
+        rv = connection.dispatch([first_message], 1)
+        p rv
+        expect(rv.documents.first['ok']).to eq(1)
+
+        ClusterTools.instance.change_primary
+
+        connection.dispatch([message], 2)
+        # No response will be returned hence we have no response assertions here
+
+        expect(connection.connected?).to be false
+      end
+    end
+  end
 end
