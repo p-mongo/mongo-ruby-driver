9c4a6877f18014854ddec81d0b10e358638eccd6
Oleg Pudeyev <oleg@bsdpower.com>
2020-03-02 01:30:03 -0500

    consolidate cleanup in a single hook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,18 +18,16 @@ RSpec.configure do |config|
   config.include(Authorization)
   config.extend(Constraints)
 
-  config.before(:all) do
-    if ClusterConfig.instance.fcv_ish >= '3.6'
-      kill_all_server_sessions
-    end
-  end
-
   # RSpec seems to run all after hooks before returning to around hooks,
   # even if around hooks are defined later in program execution.
   # This means in order for local client cleanup to not interfere with
   # global assertions (guarded via scope_expectations), the cleanup has to
   # happen in an around hook also.
   config.around do |example|
+    if ClusterConfig.instance.fcv_ish >= '3.6'
+      kill_all_server_sessions
+    end
+
     begin
       example.run
     ensure
